5cf01c37909821ff847512baa23270cc4edc2b6e
David A. Parry <david.parry@igmm.ed.ac.uk>
2019-09-17 21:12:36 +0100

    add qsub_pipeline module

---
+++ muver/qsub_pipeline.py
@@ -0,0 +1,227 @@
+import os
+import sys
+import reference
+from sample import (Sample, read_samples_from_text,
+                    generate_experiment_directory, write_sample_info_file)
+from math import ceil
+import ConfigParser
+
+PATHS = dict()
+config = ConfigParser.ConfigParser()
+config.read(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), '..', 'paths.cfg'))
+for key, value in config.items('paths'):
+    PATHS[key] = value
+
+
+class CommandGenerator(object):
+
+    def __init__(self, sample, fq1, i, ref, fq2=None, processes=1,
+                 align_mem=8, run_time=48):
+        self.sample = sample
+        self.fq1 = fq1
+        self.fq2 = fq2
+        self.ref = ref
+        self.run_time = run_time
+        self.align_mem = float(align_mem)
+        self.processes = processes
+        self.i = i
+
+    def align(self, script_name):
+        out = self.sample._mapq_filtered_sams[self.i].name
+        self.sample._mapq_filtered_sams[self.i].close()
+        if self.fq2:
+            fq_string = "-1 {} -2 {} --fr".format(self.fq1, self.fq2)
+            write_cmd = "perl -wane 'print if /^\@/ or $F[6] eq \"=\";' | "
+        else:
+            fq_string = "-U {}".format(self.fq1)
+            write_cmd = ''
+        cmd = '''
+{} {} \\
+    -q --phred33 -p {} \\
+    -I 0 -X 1000 --local --sensitive-local \\
+    -x {} | \\
+    {}{} view \\
+        -S -O BAM -q 20 \\
+        -o {} -
+'''.format(PATHS['bowtie2'], fq_string, self.processes, self.ref,
+           write_cmd, PATHS['samtools'], out)
+        q_script = '''#!/bin/bash
+#$ -e {}.stderr
+#$ -o {}.stdout
+#$ -V
+#$ -cwd
+#$ -l h_rt={}:00:00
+#$ -l h_vmem={}G
+#$ -pe sharedmem {}
+
+set -euo pipefail
+
+echo $(date) Doing alignment
+{}
+
+echo $(date) Finished
+echo $?
+'''.format(script_name, script_name, self.run_time,
+           int(ceil(self.align_mem/self.processes)), self.processes, cmd)
+        return q_script
+
+    def add_read_groups(self, script_name):
+        return 'placeholder'
+
+    def deduplicate(self, script_name):
+        return 'placeholder'
+
+    def realigner_target_creator(self, script_name):
+        return 'placeholder'
+
+    def indel_realigner(self, script_name):
+        return 'placeholder'
+
+    def fix_mate_information(self, script_name):
+        return 'placeholder'
+
+
+def generate_qsub_scripts(fq1, fq2, ref, sample, i, p=1):
+    command_gen = CommandGenerator(sample=sample, ref=ref, fq1=fq1, fq2=fq2,
+                                   i=i, processes=p)
+    directory = os.path.join(sample.exp_dir, 'qsub_scripts', '')
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+    qsub_stages = ['align', 'add_read_groups', 'deduplicate',
+                   'realigner_target_creator', 'indel_realigner',
+                   'fix_mate_information', ]
+    scripts = list()
+    for stage in qsub_stages:
+        scr = os.path.join(directory, "{}_{}_{}.sh".format(sample.sample_name,
+                                                           i, stage))
+        with open(scr, 'wt') as outfile:
+            outfile.write(getattr(command_gen, stage)(scr))
+        scripts.append(scr)
+    return scripts, sample._fixed_mates_bams[i]
+
+def run_pipeline(reference_assembly, fastq_list, control_sample,
+                 experiment_directory, p=1, excluded_regions=None,
+                 fwer=0.01, max_records=1000000):
+    '''
+    Run the MuVer pipeline considering input FASTQ files.  All files written
+    to the experiment directory.
+    '''
+    repeat_file = '{}.repeats'.format(os.path.splitext(reference_assembly)[0])
+
+    if not reference.check_reference_indices(reference_assembly):
+        sys.stderr.write('Reference assembly not indexed. Run '
+            '"muver index_reference".\n')
+        exit()
+    if not os.path.exists(repeat_file):
+        sys.stderr.write('Repeats not found for reference assembly. Run '
+            '"muver create_repeat_file".\n')
+        exit()
+    generate_experiment_directory(experiment_directory)
+    samples = read_samples_from_text(
+        fastq_list, exp_dir=experiment_directory)
+    control_sample = next(
+        (x for x in samples if x.sample_name == control_sample),
+        None,
+    )
+
+    for sample in samples:
+        sample.generate_intermediate_files()
+    # Align
+    sample2bam = dict()
+    for sample in samples:
+        for i, fastqs in enumerate(sample.fastqs):
+            if len(fastqs) == 2:
+                f1, f2 = fastqs
+            else:
+                f1 = fastqs[0]
+                f2 = None
+            scripts, final_bam = generate_qsub_scripts(fq1=f1, fq2=f2,
+                                                       ref=reference_assembly,
+                                                       sample=sample, i=i, p=p)
+        if len(sample.fastqs) > 1:
+            sample2bam[sample.sample_name] = sample.merged_bam
+            #TODO merge
+            pass
+        else:
+            sample2bam[sample.sample_name] = sample._fixed_mates_bams[0]
+
+    # Process output SAM files
+#    pool.map(process_sams, zip(
+#        [s.sample_name for s in samples],
+#        [s.get_intermediate_file_names() for s in samples],
+#        repeat(reference_assembly),
+#        repeat(max_records),
+#    ))
+#
+#    # Run HaplotypeCaller
+#    haplotype_caller_vcf = os.path.join(
+#        experiment_directory,
+#        'gatk_output',
+#        'haplotype_caller_output.vcf'
+#    )
+#    haplotype_caller_log = os.path.join(
+#        experiment_directory,
+#        'logs',
+#        'haplotype_caller.log'
+#    )
+#    bams = [s.merged_bam for s in samples]
+#    gatk.run_haplotype_caller(
+#        bams,
+#        reference_assembly,
+#        haplotype_caller_vcf,
+#        haplotype_caller_log,
+#        nct=p,
+#    )
+#
+#    chrom_sizes = reference.read_chrom_sizes(reference_assembly)
+#
+#    strand_bias_std_values = pool.map(analyze_depth_distribution, zip(
+#        range(len(samples)),
+#        [s.get_intermediate_file_names() for s in samples],
+#        repeat(reference_assembly),
+#        repeat(chrom_sizes),
+#        [s.ploidy for s in samples],
+#        [s.cnv_regions for s in samples],
+#    ))
+#    for index, strand_bias_std in strand_bias_std_values:
+#        samples[index].strand_bias_std = strand_bias_std
+#
+#    # Characterize repeats
+#
+#    if os.path.isfile(repeat_file + '.sample'):
+#        repeats = read_repeats(repeat_file + '.sample')
+#    else:
+#        repeats = read_repeats(repeat_file)
+#
+#    pool.map(characterize_repeat_indel_rates, zip(
+#        [s.get_intermediate_file_names() for s in samples],
+#        repeat(repeats),
+#        [s.repeat_indel_header for s in samples],
+#    ))
+#    for sample in samples:
+#        sample.repeat_indel_fits_dict = read_fits(sample.repeat_indel_fits)
+#
+#    variants = VariantList(
+#        haplotype_caller_vcf, samples, excluded_regions, repeat_file,
+#        control_sample, chrom_sizes, fwer)
+#
+#    text_output = os.path.join(
+#        experiment_directory,
+#        'output',
+#        'mutations.txt'
+#    )
+#    vcf_output = os.path.join(
+#        experiment_directory,
+#        'output',
+#        'mutations.vcf'
+#    )
+#    variants.write_output_table(text_output)
+#    variants.write_output_vcf(vcf_output)
+#
+#    for sample in samples:
+#        sample.clear_temp_file_indices()
+#
+#    sample_info_file = os.path.join(
+#        experiment_directory, 'sample_info.txt')
+#    write_sample_info_file(samples, sample_info_file)
